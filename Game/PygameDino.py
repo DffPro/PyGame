cc78ea52f8e89979e7d2c96e987a87c018a20c4b
DffPro <dffdffpro@yandex.ru>
2026-05-14 22:40:55 +0500

    Update PygameDino.py
    Проверка

--- Game/PygameDino.py
+++ Game/PygameDino.py
@@ -2,27 +2,42 @@ import pygame
 
 pygame.init()
 
-# определяем размеры окна
-window_size = (300, 300)
+pygame.init()
+pygame.display.set_caption('Сменность фона')
+window_surface = pygame.display.set_mode((300, 300))
+background = pygame.Surface((300, 300))
+background.fill(pygame.Color('#000000'))
 
-# задаем название окна
-pygame.display.set_caption("Синий фон")
+color_list = [
+    pygame.Color('#FF0000'),  # красный
+    pygame.Color('#00FF00'),  # зеленый
+    pygame.Color('#0000FF'),  # синий
+    pygame.Color('#FFFF00'),  # желтый
+    pygame.Color('#00FFFF'),  # бирюзовый
+    pygame.Color('#FF00FF'),  # пурпурный
+    pygame.Color('#FFFFFF')   # белый
+]
 
-# создаем окно
-screen = pygame.display.set_mode(window_size)
+current_color_index = 0
 
-# задаем цвет фона
-background_color = (0, 0, 255)  # синий
+button_font = pygame.font.SysFont('Verdana', 15) # используем шрифт Verdana
+button_text_color = pygame.Color("black")
+button_color = pygame.Color("gray")
+button_rect = pygame.Rect(100, 115, 100, 50)
+button_text = button_font.render('Нажми!', True, button_text_color)
 
-# заполняем фон заданным цветом
-screen.fill(background_color)
-
-# обновляем экран для отображения изменений
-pygame.display.flip()
-
-# показываем окно, пока пользователь не нажмет кнопку "Закрыть"
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if button_rect.collidepoint(event.pos):
+                current_color_index = (current_color_index + 1) % len(color_list)
+                background.fill(color_list[current_color_index])
+
+        window_surface.blit(background, (0, 0))
+        pygame.draw.rect(window_surface, button_color, button_rect)
+        button_rect_center = button_text.get_rect(center=button_rect.center)
+        window_surface.blit(button_text, button_rect_center)
+        pygame.display.update()
