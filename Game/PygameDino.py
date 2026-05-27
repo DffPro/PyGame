18a48962a497c6e90008fcc1abc168d788d3447e
DffPro <dffdffpro@yandex.ru>
2026-05-27 22:05:51 +0500

    Update PygameDino.py

--- Game/PygameDino.py
+++ Game/PygameDino.py
@@ -2,7 +2,18 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(1200,600)
+screen = pygame.display.set_mode((1200,600))
 pygame.display.set_caption('Dino-Reverse')
-clock = pygame.time.Clock
+clock = pygame.time.Clock()
 font = pygame.freetype.Font(None, 40)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill((0,0,0))
+    pygame.display.flip()
+    clock.tick(60)
