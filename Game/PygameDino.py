434543222b68c8fd437dab0f4c9aa83aeddc4def
DffPro <dffdffpro@yandex.ru>
2026-06-11 19:35:47 +0500

    Update PygameDino.py

--- Game/PygameDino.py
+++ Game/PygameDino.py
@@ -9,8 +9,8 @@ font = pygame.freetype.Font(None, 40)
 
 cactus_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/Cactus.jpg')
 dino_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/Dino.jpg')
-dino_Image = pygame.transform.scale(dino_Image, (50, 50))
-ground_Image = pygame.image.load('Image/ground.png')
+dino_Image = pygame.transform.scale(dino_Image, (120, 120))
+ground_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/ground.png')
 
 
 class Dino():
