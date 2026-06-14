d4d48a7e66fa44e47c3ebfd653d0a8a110ce60f0
DffPro <dffdffpro@yandex.ru>
2026-06-14 14:47:14 +0500

    Update PygameDino.py

--- Game/PygameDino.py
+++ Game/PygameDino.py
@@ -8,16 +8,17 @@ clock = pygame.time.Clock()
 
 
 cactus_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/Cactus.jpg')
-dino_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/dino.png')
+dino_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/Dino.jpg')
 dino_Image = pygame.transform.scale(dino_Image, (120, 120))
 ground_Image = pygame.image.load('C:/Users/Фрошикейн/PyGame/Game/Image/ground.png')
+ground_Image = pygame.transform.scale(ground_Image, (900, 142))
 
 ground_group = pygame.sprite.Group()
 cactus_group = pygame.sprite.Group()
 
 ground_event = pygame.USEREVENT
 cactus_event = pygame.USEREVENT +1
-pygame.time.set_timer(ground_event, 1100)
+pygame.time.set_timer(ground_event, 1000)
 
 class Ground(pygame.sprite.Sprite):
     def __init__(self, image, position):
@@ -36,7 +37,7 @@ class Dino():
         self.rect = self.image.get_rect()
         self.rect.center = position
         self.y = 0
-        
+        self.step = 5
         self.max_jump = 40
         self.in_jump = False
     
@@ -44,10 +45,10 @@ class Dino():
         if self.in_jump:
             if self.y < self.max_jump:
                 self.y += 1
-                self.rect.y -= 1
+                self.rect.y -= self.step
             elif self.y < self.max_jump * 2:
                 self.y += 1 
-                self.rect.y += 1 
+                self.rect.y += self.step
             else:
                 self.in_jump = False
                 self.y = False
@@ -55,7 +56,7 @@ class Dino():
     def draw(self):
         screen.blit(self.image, self.rect)
 
-dino = Dino(dino_Image, (100, 400))
+dino = Dino(dino_Image, (800, 500))
 
 running = True
 while running:
@@ -65,7 +66,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             dino.in_jump = True
         if event.type == ground_event:
-            g = Ground(ground_Image, (100, 450))
+            g = Ground(ground_Image, (100, 550))
             ground_group.add(g)
 
     screen.fill((0, 0, 0))
